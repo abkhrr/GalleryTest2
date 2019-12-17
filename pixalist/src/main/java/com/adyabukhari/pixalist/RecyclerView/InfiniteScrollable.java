ed8fb1a760d39df34a17f1c53dec24660f3553af
abkhrr <bukhariadbuk@gmail.com>
2019-12-18 04:25:42 +0700

    initial commit

--- pixalist/src/main/java/com/adyabukhari/pixalist/RecyclerView/InfiniteScrollable.java
+++ pixalist/src/main/java/com/adyabukhari/pixalist/RecyclerView/InfiniteScrollable.java
@@ -5,21 +5,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public abstract class InfiniteScrollable extends RecyclerView.OnScrollListener  {
 
-    /**
-     * Created by Abkhrr on 17/12/2019.
-     *
-     * Email: bukhariadbuk@gmail.com
-     * GitHub: https://github.com/abkhrr
-     *
-     */
-
     private int previousTotal = 0;
     private boolean loading = true;
-    private int image_page = 1;
+    private int current_page = 1;
 
     private final LinearLayoutManager mLinearLayoutManager;
-
-    // Set The Infinite Scroll
 
     public InfiniteScrollable(LinearLayoutManager linearLayoutManager) {
         this.mLinearLayoutManager = linearLayoutManager;
@@ -29,20 +19,20 @@ public abstract class InfiniteScrollable extends RecyclerView.OnScrollListener  
     public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
         super.onScrolled(recyclerView, dx, dy);
 
-        int VisibleCount = recyclerView.getChildCount();
-        int totalCount = mLinearLayoutManager.getItemCount();
-        int firstVisibleImage = mLinearLayoutManager.findFirstVisibleItemPosition();
+        int visibleItemCount = recyclerView.getChildCount();
+        int totalItemCount = mLinearLayoutManager.getItemCount();
+        int firstVisibleItem = mLinearLayoutManager.findFirstVisibleItemPosition();
 
-        if (loading && (VisibleCount > previousTotal)) {
+        if (loading && (totalItemCount > previousTotal)) {
             loading = false;
-            previousTotal = totalCount;
+            previousTotal = totalItemCount;
         }
 
         int visibleThreshold = 5;
-        if (!loading && (totalCount - VisibleCount)
-                <= (firstVisibleImage + visibleThreshold)) {
-            image_page++;
-            onLoadMore(image_page);
+        if (!loading && (totalItemCount - visibleItemCount)
+                <= (firstVisibleItem + visibleThreshold)) {
+            current_page++;
+            onLoadMore(current_page);
             loading = true;
         }
     }
