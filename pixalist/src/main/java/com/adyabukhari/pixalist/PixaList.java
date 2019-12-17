517e7c9a3c7df22ea8c317f82f58e37e21a1c97f
abkhrr <bukhariadbuk@gmail.com>
2019-12-17 21:30:21 +0700

    second commit

--- pixalist/src/main/java/com/adyabukhari/pixalist/PixaList.java
+++ pixalist/src/main/java/com/adyabukhari/pixalist/PixaList.java
@@ -2,9 +2,7 @@ package com.adyabukhari.pixalist;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
